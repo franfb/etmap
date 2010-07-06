122a0cc70a53c01db23e8a464ab14f2815c32685
franfumero <franfumero@40b908a8-6121-b738-4ac3-2161f4cfbe0f>
2010-07-06 17:30:54 +0000

    Limpieza del fichero quitando líneas comentadas de código no utilizado.

--- etmap/src/etmap/modis/LstData.java
+++ etmap/src/etmap/modis/LstData.java
@@ -15,9 +15,6 @@ public class LstData {
 		this.dimX = dimX;
 		this.dimY = dimY;
 		this.scaleFactor = scaleFactor;
-//		data = new short[dimX * dimY];
-//		lat = new double[dimX * dimY];
-//		lon = new double[dimX * dimY];
 	}
 
 	public int getDimX() {
@@ -34,10 +31,6 @@ public class LstData {
 
 	public void setData(short[] data) {
 		this.data = data;
-//		for (int y = 0; y < dimX; y++)
-//			for (int x = 0; x < dimY; x++) {
-//				this.data[y * dimY + x] = data[y * dimY + x] * scaleFactor;
-//			}
 	}
 
 	public short[] getData() {
@@ -101,25 +94,14 @@ public class LstData {
 		double x_interp = (lo - LstConstants.VLLON)
 				/ (LstConstants.DELTA * (LstConstants.VRLON - LstConstants.VLLON));
 		
-//		System.out.println("Y_interp = " + y_interp);
-//		System.out.println("X_interp = " + x_interp);
-		
 		int x_izq = (int) x_interp;
 		int x_der = x_izq + 1;
 		int y_sup = (int) y_interp;
 		int y_inf = y_sup + 1;
 		
-//		System.out.println("x_izq = " + x_izq);
-//		System.out.println("x_der = " + x_der);
-//		System.out.println("y_inf = " + y_inf);
-//		System.out.println("y_sup = " + y_sup);
-		
 		// Calculamos las posiciones a interpolar en un cuadrado de lado = 1
 		x_interp = x_interp - x_izq;
 		y_interp = y_inf - y_interp;
-		
-//		System.out.println("Y_interp = " + y_interp);
-//		System.out.println("X_interp = " + x_interp);
 		
 		double temp_interp = 0.0;
 		// Interpolamos la temperatura a partir de las parejas (x,y) si todas las temperaturas no son 0
@@ -129,106 +111,11 @@ public class LstData {
 			temp_interp = bilinear(0, 1, x_interp, 0, 1, y_interp, 
 					getScaledData(x_izq, y_inf), getScaledData(x_der, y_inf), 
 					getScaledData(x_izq, y_sup), getScaledData(x_der, y_sup));
-
-//			System.out.println("Punto a interpolar:");
-//			System.out.println("X = " + x_interp);
-//			System.out.println("Y = " + y_interp);
-//			System.out.println("x_izq = " + x_izq);
-//			System.out.println("x_der = " + x_der);
-//			System.out.println("y_inf = " + y_inf);
-//			System.out.println("y_sup = " + y_sup);
-//
-//			System.out.println("Latitudes:");
-//			System.out.println("lat1 = " + getLatLngByPos(x_izq, y_sup)[0]);
-//			System.out.println("lat = " + lat);
-//			System.out.println("lat2 = " + getLatLngByPos(x_izq, y_inf)[0]);
-//			
-//			System.out.println("Longitudes:");
-//			System.out.println("lon1 = " + getLatLngByPos(x_der, y_sup)[1]);
-//			System.out.println("lon = " + lon);
-//			System.out.println("lon2 = " + getLatLngByPos(x_izq, y_sup)[1]);
-//			System.out.println("Temperatura y_sup,x_der = "
-//					+ (getScaledData(x_der, y_sup) - 273.15));
-//			System.out.println("Temperatura y_sup,x_izq = "
-//					+ (getScaledData(x_izq, y_sup) - 273.15));
-//			System.out.println("Temperatura y_inf,x_der = "
-//					+ (getScaledData(x_der, y_inf) - 273.15));
-//			System.out.println("Temperatura y_inf,x_izq = "
-//					+ (getScaledData(x_izq, y_inf) - 273.15));
-//			System.out.println("Temperatura interpolada = "
-//					+ (temp_interp - 273.15));
 		}
+
 		return temp_interp;
 	}
 	
-//	public double getInterpolatedTemperature(double lat, double lon) {
-//		// Buscamos la latitud m�s pr�xima a la solicitada
-//		int y_sup, y_inf;
-//		int y = 0;
-//		while ((y < dimY) && (lat <= this.lat[y * dimY])) {
-//			y++;
-//		}
-//		y_sup = y - 1;
-//		y_inf = y;
-//
-//		System.out.println("Latitudes:");
-//		System.out.println("lat1 = " + this.lat[y_sup * dimY]);
-//		System.out.println("lat = " + lat);
-//		System.out.println("lat2 = " + this.lat[y_inf * dimY]);
-//		
-//		// Buscamos la longitud m�s pr�xima a la solicitada
-//		int x_der, x_izq;
-//		int x = dimX - 1;
-//		while ((x >= 0) && (lon <= this.lon[y_sup * dimY + x])) {
-//			x--;
-//		}
-//		x_der = x + 1;
-//		x_izq = x;
-//		
-//		double temp_interp = 0.0;
-//		// Interpolamos la temperatura a partir de las parejas (latX,lonX) si todas las temperaturas no son 0
-//		if ((data[y_inf * dimY + x_izq] != 0.0) && (data[y_inf * dimY + x_der] != 0.0) && 
-//				(data[y_sup * dimY + x_izq] != 0.0) && (data[y_sup * dimY + x_der] != 0.0)) {
-//			// Calculamos las coordenadas cartesianas del punto que necesitamos
-//			double y_interp = (lat - this.lat[y_inf * dimY])
-//					/ (this.lat[y_sup * dimY] - this.lat[y_inf * dimY]);
-//			// lo = x * delta * (vrlon - vllon) + vllon;
-//			// lon[y * dimY + x] = (lo / (r * Math.cos(la / r))) * (180.0 / pi);
-//			double lo = (lon * (Math.PI / 180.0))
-//					* (LstConstants.EARTH_RADIUS * Math.cos(lat
-//							* (Math.PI / 180.0)));
-//			double x_interp = (lo - LstConstants.VLLON)
-//					/ (LstConstants.DELTA * (LstConstants.VRLON - LstConstants.VLLON))
-//					- x_izq;
-//			temp_interp = bilinear(0, 1, x_interp, 0, 1, y_interp, 
-//					data[y_inf * dimY + x_izq], data[y_inf * dimY + x_der], 
-//					data[y_sup * dimY + x_izq], data[y_sup * dimY + x_der]);
-//
-//			System.out.println("Punto a interpolar:");
-//			System.out.println("X = " + x_interp);
-//			System.out.println("Y = " + y_interp);
-//			System.out.println("x_izq = " + x_izq);
-//			System.out.println("x_der = " + x_der);
-//			System.out.println("y_inf = " + y_inf);
-//			System.out.println("y_sup = " + y_sup);
-//
-//			System.out.println("Longitudes:");
-//			System.out.println("lon1 = " + this.lon[y_sup * dimY + x_der]);
-//			System.out.println("lon = " + lon);
-//			System.out.println("lon2 = " + this.lon[y_sup * dimY + x_izq]);
-//			System.out.println("Temperatura y_sup,x_der = "
-//					+ (data[y_sup * dimY + x_der] - 273.15));
-//			System.out.println("Temperatura y_sup,x_izq = "
-//					+ (data[y_sup * dimY + x_izq] - 273.15));
-//			System.out.println("Temperatura y_inf,x_der = "
-//					+ (data[y_inf * dimY + x_der] - 273.15));
-//			System.out.println("Temperatura y_inf,x_izq = "
-//					+ (data[y_inf * dimY + x_izq] - 273.15));
-//			System.out.println("Temperatura interpolada = "
-//					+ (temp_interp - 273.15));
-//		}
-//		return temp_interp;
-//	}
 
 	private static double bilinear(double x1, double x2, double x, double y1,
 			double y2, double y, double q11, double q21, double q12, double q22) {
