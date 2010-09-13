a07df8b982f73f7e9d363ad8190218c23183b67b
franfumero <franfumero@40b908a8-6121-b738-4ac3-2161f4cfbe0f>
2010-09-13 19:18:55 +0000

    Implementada la función buscarIntervalo (entre dos fechas) de la clase LeerHdf.

---
+++ trunk/Capa1/Principal/src/FuncionesCapa2/LeerHdf.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FuncionesCapa2;
+
+import FuncionesCapa1.CapturaDatos;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Fran
+ */
+public class LeerHdf {
+
+    private static String SAT_TERRA = "MOD11A1";
+    private static String SAT_AQUA = "MYD11A1";
+    private static String GRANULO = "h16v06";
+    CapturaDatos datos;
+
+    public LeerHdf() {
+        String directorio_local_de_hdfs = "d:\\etsii\\pfc\\hdfs\\";
+        String ftp_de_hdfs = "e4ftl01u.ecs.nasa.gov";
+        datos = new CapturaDatos(directorio_local_de_hdfs, ftp_de_hdfs, 21, "anonymous", "anonymous");
+    }
+
+    /**
+     * Busca los ficheros HDF correspondientes al intervalo de fechas 
+     * solicitado. El intervalo se especifica mediante una fecha de referencia y 
+     * los días hacia atrás, sin contar el día de referencia. Por ejemplo, si 
+     * especificamos el día 10-04-2010 y 4 días hacia trás, buscará los HDF 
+     * desde el día 06 hasta el día 10 de abril de 2010, ambos inclusive.
+     * 
+     * @param dia Día del mes de la fecha de referencia.
+     * @param mes Mes de la fecha referencia.
+     * @param ano Año de la fecha referencia.
+     * @param diasHaciaAtras Días hacia atrás desde la fecha de referencia que 
+     * permiten especificar el intervalo de fechas solicitado.
+     * @see #buscarIntervalo(int, int, int, int, int, int) 
+     */
+    public void buscarIntervalo(int dia, int mes, int ano, int diasHaciaAtras) {
+    }
+
+    /**
+     * Busca los ficheros HDF correspondientes al intervalo de fechas
+     * solicitado. El intervalo se especifica mediante una fecha
+     * inicial y una fecha final. Por ejemplo, si especificamos el día
+     * 06-04-2010 como fecha inicial y el día 10-04-2010 como fecha final,
+     * se buscarán los HDF desde el día 06 hasta el día 10 de abril de 2010,
+     * ambos inclusive.
+     *
+     * @param diaIni Día del mes de la fecha inicial.
+     * @param mesIni Mes de la fecha inicial.
+     * @param anoIni Año de la fecha inicial.
+     * @param diaFin Día del mes de la fecha final.
+     * @param mesFin Mes de la fecha final.
+     * @param anoFin Año de la fecha final.
+     * @see #buscarIntervalo(int, int, int, int)
+     */
+    public void buscarIntervalo(int diaIni, int mesIni, int anoIni, int diaFin, int mesFin, int anoFin) {
+        Calendar orig = Calendar.getInstance();
+        orig.set(anoIni, mesIni - 1, diaIni);  // Enero == 0
+
+        Calendar fin = Calendar.getInstance();
+        fin.set(anoFin, mesFin - 1, diaFin);
+
+        SimpleDateFormat FormatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+        while (orig.compareTo(fin) <= 0) {
+            String nombre = datos.obtenerHdfLocal(SAT_AQUA, GRANULO, FormatoFecha.format(orig.getTime()));
+            if (nombre == null) {
+                nombre = datos.obtenerHdfFTP(SAT_AQUA, GRANULO, FormatoFecha.format(orig.getTime()));
+            }
+            orig.add(Calendar.DAY_OF_MONTH, 1);
+        }
+    }
+}
--- trunk/Capa1/Principal/src/principal/Main.java
+++ trunk/Capa1/Principal/src/principal/Main.java
@@ -6,21 +6,49 @@
 package principal;
 
 import FuncionesCapa1.*;
+import FuncionesCapa2.LeerHdf;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Calendar;
+
 
 public class Main {
     /**
      * @param args the command line arguments
+     *
+     *
+     * Para usar un rango de fechas:
+     * Calendar fecha = Calendar.getInstance();
+           fecha.set(Calendar.DAY_OF_MONTH, dia_del_mes);
+           fecha.set(Calendar.MONTH, mes);
+           fecha.set(Calendar.YEAR, año);
+     *
+     * fecha.set(Calendar.DAY_OF_YEAR, fecha.get(Calendar.DAY_OF_YEAR + 1));
+     *
+     * Calendar origDay = Calendar.getInstance();
+System.out.println ("Original Date: " + origDay.getTime());
+
+Calendar prevDay = (Calendar) origDay.clone();
+prevDay.add (Calendar.DAY_OF_YEAR, -1);
+System.out.println ("Previous Day: " + prevDay.getTime());
+
+Calendar nextDay = (Calendar) origDay.clone();
+nextDay.add (Calendar.DAY_OF_YEAR, 1);
+System.out.println ("Next Day: " + nextDay.getTime());
+}
+     *
      */
     public static void main(String[] args) {
-        String directorio_local_de_hdfs = "c:\\proy\\hdfs\\";
+        String directorio_local_de_hdfs = "d:\\etsii\\pfc\\hdfs\\";
         String ftp_de_hdfs = "e4ftl01u.ecs.nasa.gov";
 
         String satelite_usado = "MOD11A1";
         String granulo_estudiado = "h16v06";
         String fecha = "07/07/2010";
+//        Calendar orig = Calendar.getInstance();
+//        LeerHdf leer = new LeerHdf();
+//        leer.
 
         CapturaDatos Datos = new CapturaDatos(directorio_local_de_hdfs, ftp_de_hdfs, 21, "anonymous", "anonymous");
         String nombre = Datos.obtenerHdfLocal(satelite_usado, granulo_estudiado, fecha);
